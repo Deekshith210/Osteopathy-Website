8b6579f277797344e0838f76824ed918e278ca04
Deekshith <deekshithmatam02@gmail.com>
2025-12-09 18:46:24 +0530

    Add Firebase initialization to index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNstfCpV9tzwGMg9TlbQNzM53sHbpMG3Q",
+  authDomain: "bhuvan-osteopath.firebaseapp.com",
+  projectId: "bhuvan-osteopath",
+  storageBucket: "bhuvan-osteopath.firebasestorage.app",
+  messagingSenderId: "251244406082",
+  appId: "1:251244406082:web:4ae2623208f72abb6b14db",
+  measurementId: "G-RBE1W3GJGK"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
